d9547633e7066e8a010f37234143b83b0cdab60c
Fatman452 <franklintoribio0702@gmail.com>
2021-08-05 15:28:18 -0400

    1 file modified

--- Map.jsx
+++ Map.jsx
@@ -1,22 +1,40 @@
 /* global google */
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
-
+const provincias = {
+  dajabon: [
+    { lat: 18.5, lng: -69.5 },
+    { lat: 18.5, lng: -69.327 },
+    { lat: 18.9, lng: -69.327 },
+    { lat: 18.9, lng: -69.5 }
+  ],
+  'santo domingo': [
+    { lat: 18.5, lng: -70.5 },
+    { lat: 18.5, lng: -71.327 },
+    { lat: 18.9, lng: -71.327 },
+    { lat: 18.9, lng: -70.5 }
+  ]
+};
 const apiIsLoaded = (map, maps, center) => {
-  const circle = new maps.Polygon({
-    strokeColor: '#FF0000',
-    strokeOpacity: 0.8,
-    strokeWeight: 1,
-    fillColor: '#FF0000',
-    fillOpacity: 0.3,
-    map,
-    path: [
-      { lat: 17.7009047, lng: -71.8 },
-      { lat: 17.7009047, lng: -68.327 },
-      { lat: 20, lng: -68.327 },
-      { lat: 20, lng:-71.8 }
-    ]
-  });
+  const info = new maps.InfoWindow();
+
+  for (let prov of Object.values(provincias)) {
+    const province_shape = new maps.Polygon({
+      strokeColor: '#FF0000',
+      strokeOpacity: 0.8,
+      strokeWeight: 1,
+      fillColor: '#FF0000',
+      fillOpacity: 0.3,
+      map,
+      path: prov
+    });
+    province_shape.addListener('click', event => {
+      console.log(province_shape);
+      info.setContent('Content');
+      info.setPosition(event.latLng);
+      info.open(map);
+    });
+  }
 };
 
 const googleAPIKey = '';
